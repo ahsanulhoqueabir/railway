75c624426303cc60b260e211cb084bddd8cd8c5d
ahsanulhoqueabir <contact.ahsanul@gmail.com>
2025-04-12 21:51:23 +0600

    fix: remove redundant comment in password hashing function

--- api/utility/hasspass.js
+++ api/utility/hasspass.js
@@ -2,7 +2,7 @@ import bcrypt from "bcryptjs";
 
 async function encryptPassword(password) {
   const salt = await bcrypt.genSalt(10); // Generate a salt
-  const hashedPassword = await bcrypt.hash(password, salt); // Hash the password
+  const hashedPassword = await bcrypt.hash(password, salt);
   return hashedPassword;
 }
 
